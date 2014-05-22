750084a3ebdcf0f0aaee0ad2503110c6526cc55f
Erwin van Eyk <Erwin_dso@hotmail.com>
2014-05-22 17:33:00 +0200

    Prevent duplicate default features

--- core/src/main/java/com/crawljax/core/state/duplicatedetection/NearDuplicateDetectionSingleton.java
+++ core/src/main/java/com/crawljax/core/state/duplicatedetection/NearDuplicateDetectionSingleton.java
@@ -3,20 +3,17 @@ package com.crawljax.core.state.duplicatedetection;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
 public class NearDuplicateDetectionSingleton {
 	private static NearDuplicateDetection ndd;
 	private static int threshold;
 	private static List<FeatureType> features = new ArrayList<FeatureType>();
 	
-	private static final Logger logger = LoggerFactory.getLogger(NearDuplicateDetectionCrawlHash32.class);
+	static {
+		features.add(new FeatureShingles(3, FeatureSizeType.WORDS));
+	}
 	
 	public static NearDuplicateDetection getInstance() {
 		if (ndd == null) {
-			features.add(new FeatureShingles(3, FeatureSizeType.WORDS));
 			ndd = new NearDuplicateDetectionCrawlHash32(threshold, features);
 		}
 		return ndd;
