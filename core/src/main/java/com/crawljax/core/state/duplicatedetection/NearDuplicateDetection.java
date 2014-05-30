c759f428b3fa4486439ed38c089a21d686798d2e
wilco van leeuwen <wilcovleeuwen@gmail.com>
2014-05-30 10:52:46 +0200

    Used crawljax-formatter for duplicate detection package

--- core/src/main/java/com/crawljax/core/state/duplicatedetection/NearDuplicateDetection.java
+++ core/src/main/java/com/crawljax/core/state/duplicatedetection/NearDuplicateDetection.java
@@ -1,17 +1,13 @@
 package com.crawljax.core.state.duplicatedetection;
 
 /**
- * Settings:
- * - Features
- * - Feature weights
- * - Threshold
- *
+ * Settings: - Features - Feature weights - Threshold
  */
 public interface NearDuplicateDetection {
-	
+
 	public int[] generateHash(String doc) throws FeatureException;
 
 	public boolean isNearDuplicateHash(int[] hash1, int[] hash2);
-		
+
 	public double getDistance(int[] hash1, int[] hash2);
 }
--- core/src/main/java/com/crawljax/core/state/duplicatedetection/NearDuplicateDetectionSingleton.java
+++ core/src/main/java/com/crawljax/core/state/duplicatedetection/NearDuplicateDetectionSingleton.java
@@ -12,52 +12,62 @@ public class NearDuplicateDetectionSingleton {
 	private static double threshold;
 	private static List<FeatureType> features = new ArrayList<FeatureType>();
 	private static HashGeneratorFactory hashGeneratorFactory = new XxHashGeneratorFactory();
-	
+
 	/**
 	 * Retrieves, if necessary creates, an instance
+	 * 
 	 * @return near-duplicate detection instance
 	 */
 	public static NearDuplicateDetection getInstance() {
 		if (ndd == null) {
-			ndd = new NearDuplicateDetectionBroder32(threshold, features, hashGeneratorFactory.getInstance());
+			ndd =
+			        new NearDuplicateDetectionBroder32(threshold, features,
+			                hashGeneratorFactory.getInstance());
 		}
 		return ndd;
 	}
-	
+
 	/**
 	 * Sets new hash-generator-factory.
-	 * @param hgf new HashGeneratorFactory
+	 * 
+	 * @param hgf
+	 *            new HashGeneratorFactory
 	 */
 	public static void setHashGeneratorFactory(HashGeneratorFactory hgf) {
 		hashGeneratorFactory = hgf;
 	}
-	
+
 	/**
 	 * Sets the threshold and reconstructs the instance
-	 * @param t the new threshold
+	 * 
+	 * @param t
+	 *            the new threshold
 	 */
 	public static void setThreshold(double t) {
 		threshold = t;
 		resetInstance();
 	}
-	
+
 	/**
 	 * Returns the threshold
+	 * 
 	 * @return current threshold
 	 */
 	public static double getThreshold() {
 		return threshold;
 	}
-	
+
 	/**
 	 * Adds an additional feature and reconstructs the instance
-	 * @param ft the feature to be added. 
+	 * 
+	 * @param ft
+	 *            the feature to be added.
 	 */
 	public static void addFeature(FeatureType ft) {
 		features.add(ft);
 		resetInstance();
 	}
-	
+
 	/**
 	 * Resets/rebuilds the instance stored by the singleton.
 	 */
